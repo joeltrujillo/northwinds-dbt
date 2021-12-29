956959acba429b7aae3257a19212ff155b4b2ca1
azimmerman@metlifeequityinvestors.com <azimmerman@metlifeequityinvestors.com>
2021-12-29 23:53:38 +0000

    updated stg_hubspot_companies and stg_hubspot_contacts files

--- models/staging/hubspot/stg_hubspot_contacts.sql
+++ models/staging/hubspot/stg_hubspot_contacts.sql
@@ -2,7 +2,7 @@ WITH contacts as (
     SELECT * FROM {{ source('HUBSPOT', 'CONTACTS')}}
 ),
 companies as (
-    SELECT * FROM stg_hubspot_companies
+    SELECT * FROM dbt_azimmerman.stg_hubspot_companies
 ),
 renamed as (
     SELECT CONCAT('hubspot-', HUBSPOT_ID) as contact_ID, 
@@ -13,7 +13,7 @@ renamed as (
         THEN CONCAT('(',SUBSTRING(translate(phone,'(,), ,-,.', ''),1,3),')',' ',SUBSTRING(translate(phone,'(,), ,-,.', ''),4,3),'-',SUBSTRING(translate(phone,'(,), ,-,.', ''),7,4))
     END
     as phone, 
-    business_name,
+    contacts.business_name,
     COMPANY_ID
     FROM contacts JOIN companies ON companies.business_name = contacts.business_name
    )
